294f1a7162e13cb8903345e11f271144ed12a085
anhnguyen21 <anhanh5811@gmail.com>
2021-10-10 04:58:32 +0700

    Update footer and perfect service

--- js/main.js
+++ js/main.js
@@ -11,18 +11,18 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("perfectservices__mySlides");
+  var dots = document.getElementsByClassName("perfect__dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" activeperfect", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " activeperfect";
 }
 (function($) {
 
